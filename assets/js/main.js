e89d4c4a6e9064c785ebf04bab49e1a6eda8b419
Opeyemi Agbeja <niyola59@gmail.com>
2017-03-01 14:13:57 +0100

    added date picker plugin to profile-content, changes to the overlay to be staic, mobile responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,86 +2,89 @@
 
 
 /*default actions when the DOM loads */
-$(".sidebar").css("width","100");
+$('#date-picker').datepicker();
+
+
+$(".sidebar").css("width", "100");
 $(".answer-content, .billing-content, .profile-content, .hide-content").hide();
 
 
 /*transitions for the sidebar hover */
-$(".sidebar").hover(function(){
+$(".sidebar").hover(function() {
 
-    $(".sidebar").stop().animate({width: '260px'});
-    $(".hide-content").show();
+        $(".sidebar").stop().animate({
+            width: '260px'
+        });
+        $(".hide-content").show();
     },
-    function(){
-       
-     $(".sidebar").stop().animate({width: '100px'});
+    function() {
+
+        $(".sidebar").stop().animate({
+            width: '100px'
+        });
         $(".hide-content").hide();
         //$('.nav li').removeClass('active');
-         if ( $(".profile-content").is(":visible") || $(".answer-content").is(":visible") || $(".billing-content").is(":visible")) {
-        $(".sidebar").stop().width('260');
-         $(".hide-content").show();
-      }
-      else{
-        $(".sidebar").width('100');
-        $(".hide-content").hide();
-        $('.nav li').removeClass('active');
-    }  
-});
-  
+        if ($(".profile-content").is(":visible") || $(".answer-content").is(":visible") || $(".billing-content").is(":visible")) {
+            $(".sidebar").stop().width('260');
+            $(".hide-content").show();
+        } else {
+            $(".sidebar").width('100');
+            $(".hide-content").hide();
+            $('.nav li').removeClass('active');
+        }
+    });
+
 /*transitions when sidebar link is clicked */
 
-$(function () {
-	$('#btn-profile').click( function() {
+$(function() {
+    $('#btn-profile').click(function() {
         $(".answer-content, .billing-content, .fake-width, .overlay-content").hide();
-		if( $(".profile-content").is(":hidden")){
-			$('.overlay-content').foggy({
-		     	opacity: 1.5,
-		     });
-			$(".profile-content").animate({
-				'width': 'toggle'
-			});
-            $(".overlay-content").fadeIn(1000);
-		}
-		else{
-            $('.overlay-content').css('filter', 'unset').fadeIn();
-			$(".profile-content").hide();
-            $(".fake-width").show();
-		}
-
-	});
-
-	$('#btn-answer').click( function() {
-        $(".profile-content, .billing-content, .fake-width, .overlay-content").hide();
-        if( $(".answer-content").is(":hidden")){
+        if ($(".profile-content").is(":hidden")) {
             $('.overlay-content').foggy({
                 opacity: 1.5,
-             });
+            });
+            $(".profile-content").animate({
+                'width': 'toggle'
+            });
+            $(".overlay-content").show();
+        } else {
+            $('.overlay-content').css('filter', 'unset').show();
+            $(".profile-content").hide();
+            $(".fake-width").show();
+        }
+
+    });
+
+    $('#btn-answer').click(function() {
+        $(".profile-content, .billing-content, .fake-width, .overlay-content").hide();
+        if ($(".answer-content").is(":hidden")) {
+            $('.overlay-content').foggy({
+                opacity: 1.5,
+            });
             $(".answer-content").animate({
                 'width': 'toggle'
             });
-            $(".overlay-content").fadeIn(1000);
-        }
-        else{
-            $('.overlay-content').css('filter', 'unset').fadeIn();
+            $(".overlay-content").show();
+        } else {
+            $('.overlay-content').css('filter', 'unset').show();
             $(".answer-content").hide();
             $(".fake-width").show();
         }
 
     });
 
-	$('#btn-billing').click( function() {
+    $('#btn-billing').click(function() {
         $(".answer-content, .profile-content, .fake-width, .overlay-content").hide();
-        if( $(".billing-content").is(":hidden")){
+        if ($(".billing-content").is(":hidden")) {
             $('.overlay-content').foggy({
                 opacity: 1.5,
-             });
+            });
             $(".billing-content").animate({
                 'width': 'toggle'
             });
-            $(".overlay-content").fadeIn(1000);
-        }
-        else{
-            $('.overlay-content').css('filter', 'unset').fadeIn();
+            $(".overlay-content").show();
+        } else {
+            $('.overlay-content').css('filter', 'unset').show();
             $(".billing-content").hide();
             $(".fake-width").show();
         }
@@ -92,50 +95,47 @@ $(function () {
 
 /*transitions for mobile-view when clicked */
 
-$(function () {
-    $('#btn-mobile-profile').click( function() {
+$(function() {
+    $('#btn-mobile-profile').click(function() {
         $(".answer-content, .billing-content").hide();
-        if( $(".profile-content").is(":hidden")){
+        if ($(".profile-content").is(":hidden")) {
             $(".overlay-content").hide();
 
-            
+
             $(".profile-content").animate({
                 'width': 'toggle'
             });
-        }   
-        else{
+        } else {
             $(".profile-content").hide();
             $(".overlay-content").fadeIn();
         }
 
     });
-  
-    $('#btn-mobile-answers').click( function() {
+
+    $('#btn-mobile-answers').click(function() {
         $(".profile-content, .billing-content").hide();
-        if( $(".answer-content").is(":hidden")){
+        if ($(".answer-content").is(":hidden")) {
             $(".overlay-content").hide();
-            
+
             $(".answer-content").animate({
                 'width': 'toggle'
             });
-        }
-        else{
+        } else {
             $(".answer-content").hide();
             $(".overlay-content").fadeIn();
         }
 
     });
 
-    $('#btn-mobile-billing').click( function() {
+    $('#btn-mobile-billing').click(function() {
         $(".profile-content, .answer-content").hide();
-        if( $(".billing-content").is(":hidden")){
+        if ($(".billing-content").is(":hidden")) {
             $(".overlay-content").hide();
-            
+
             $(".billing-content").animate({
                 'width': 'toggle'
             });
-        }
-        else{
+        } else {
             $(".billing-content").hide();
             $(".overlay-content").fadeIn();
         }
@@ -146,18 +146,17 @@ $(function () {
 
 /* browser resize and mobile responsiveness */
 
-  $(window).on('resize', function(event){
-    var windowSize = $(window).width(); 
-//when a user on a mobile view clicks on a link and resizes to web view, we want to hide the mobile view and switch to web view
-    if (windowSize > 960 && $(".profile-content, .answer-content, .billing-content").is(":visible")){
-       $(".profile-content, .answer-content, .billing-content").hide();
-            $(".overlay-content").css('filter', 'unset').fadeIn();
+$(window).on('resize', function(event) {
+    var windowSize = $(window).width();
+    //when a user on a mobile view clicks on a link and resizes to web view, we want to hide the mobile view and switch to web view
+    if (windowSize > 960 && $(".profile-content, .answer-content, .billing-content").is(":visible")) {
+        $(".profile-content, .answer-content, .billing-content").hide();
+        $(".overlay-content").css('filter', 'unset').fadeIn();
     }
-//when a user on a web view clicks on a link and resizes to mobile view. we want to hide the overlay-content and switch to mobile view.
-    else if(windowSize < 960 && $(".profile-content,.answer-content,.billing-content").is(":visible") && $(".overlay-content") .is(":visible")){
+    //when a user on a web view clicks on a link and resizes to mobile view. we want to hide the overlay-content and switch to mobile view.
+    else if (windowSize < 960 && $(".profile-content,.answer-content,.billing-content").is(":visible") && $(".overlay-content").is(":visible")) {
         $(".overlay-content").hide();
-    } 
-    else{
+    } else {
 
     }
 });
@@ -172,6 +171,6 @@ $('.nav li a').click(function(e) {
         $parent.addClass('active');
     }
     e.preventDefault();
-});  
+});
 
-  /*animation and transitions for the DOM ENDS*/
+/*animation and transitions for the DOM ENDS*/
